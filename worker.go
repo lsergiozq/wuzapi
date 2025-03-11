f65bc922310937eb1162f73c46ac1434a6fb56b1
lsergiozq <lsergio@lssistemas.com>
2025-03-10 21:33:11 -0300

    Update worker.go

--- worker.go
+++ worker.go
@@ -53,93 +53,92 @@ func processQueue(queue *RabbitMQQueue, s *server, cancelChan <-chan struct{}) {
 				continue
 			}
 
-			for msg := range msgs {
-				// Decodifica a mensagem JSON da fila
-				var msgData struct {
-					Id       string          `json:"Id"`
-					Phone    string          `json:"Phone"`
-					MsgProto json.RawMessage `json:"MsgProto"` // Armazena a mensagem como JSON bruto
-					Userid   int             `json:"Userid"`
-				}
+			msg := msgs
+			// Decodifica a mensagem JSON da fila
+			var msgData struct {
+				Id       string          `json:"Id"`
+				Phone    string          `json:"Phone"`
+				MsgProto json.RawMessage `json:"MsgProto"` // Armazena a mensagem como JSON bruto
+				Userid   int             `json:"Userid"`
+			}
 
-				if err := json.Unmarshal(msg.Body, &msgData); err != nil {
-					log.Error().Msg("Erro ao decodificar mensagem da fila: " + err.Error())
-					continue
-				}
+			if err := json.Unmarshal(msg.Body, &msgData); err != nil {
+				log.Error().Msg("Erro ao decodificar mensagem da fila: " + err.Error())
+				continue
+			}
 
-				if msgData.Userid == 0 {
-					log.Error().Msg("Nenhuma sessão ativa no WhatsApp")
-					continue
-				}
+			if msgData.Userid == 0 {
+				log.Error().Msg("Nenhuma sessão ativa no WhatsApp")
+				continue
+			}
 
-				// Decodifica `msgProto` diretamente da fila
-				var msgProto waProto.Message
-				if err := json.Unmarshal(msgData.MsgProto, &msgProto); err != nil {
-					log.Error().Msg("Erro ao decodificar MsgProto: " + err.Error())
-					continue
-				}
+			// Decodifica `msgProto` diretamente da fila
+			var msgProto waProto.Message
+			if err := json.Unmarshal(msgData.MsgProto, &msgProto); err != nil {
+				log.Error().Msg("Erro ao decodificar MsgProto: " + err.Error())
+				continue
+			}
 
-				recipient, ok := parseJID(msgData.Phone)
-				if !ok {
-					log.Error().Msg("Erro ao converter telefone para JID")
-					continue
-				}
+			recipient, ok := parseJID(msgData.Phone)
+			if !ok {
+				log.Error().Msg("Erro ao converter telefone para JID")
+				continue
+			}
 
-				// Envia a mensagem e captura o resultado
-				resp, err := clientPointer[msgData.Userid].SendMessage(context.Background(), recipient, &msgProto, whatsmeow.SendRequestExtra{ID: msgData.Id})
+			// Envia a mensagem e captura o resultado
+			resp, err := clientPointer[msgData.Userid].SendMessage(context.Background(), recipient, &msgProto, whatsmeow.SendRequestExtra{ID: msgData.Id})
 
-				// Define status e detalhes do envio
-				status := "success"
-				details := "Mensagem enviada com sucesso"
-				timestamp := int64(0)
+			// Define status e detalhes do envio
+			status := "success"
+			details := "Mensagem enviada com sucesso"
+			timestamp := int64(0)
 
-				if err != nil {
-					status = "error"
-					details = err.Error()
+			if err != nil {
+				status = "error"
+				details = err.Error()
+			} else {
+				timestamp = resp.Timestamp.Unix()
+			}
+
+			// Obtém o webhook do usuário
+			webhookurl := ""
+			myuserinfo, found := userinfocache.Get(s.getTokenByUserId(msgData.Userid))
+			if found {
+				webhookurl = myuserinfo.(Values).Get("Webhook")
+			}
+
+			if webhookurl != "" {
+				events := strings.Split(myuserinfo.(Values).Get("Events"), ",")
+
+				// Após o envio, verificar se o webhook deve ser chamado
+				if !Find(events, "CallBack") && !Find(events, "All") {
+					log.Warn().Msg("Usuário não está inscrito para CallBack. Ignorando webhook.")
 				} else {
-					timestamp = resp.Timestamp.Unix()
-				}
-
-				// Obtém o webhook do usuário
-				webhookurl := ""
-				myuserinfo, found := userinfocache.Get(s.getTokenByUserId(msgData.Userid))
-				if found {
-					webhookurl = myuserinfo.(Values).Get("Webhook")
-				}
-
-				if webhookurl != "" {
-					events := strings.Split(myuserinfo.(Values).Get("Events"), ",")
-
-					// Após o envio, verificar se o webhook deve ser chamado
-					if !Find(events, "CallBack") && !Find(events, "All") {
-						log.Warn().Msg("Usuário não está inscrito para CallBack. Ignorando webhook.")
-					} else {
-						// Criar estrutura de evento no mesmo formato do wmiau.go
-						postmap := map[string]interface{}{
-							"type": "CallBack",
-							"event": map[string]interface{}{
-								"id":        msgData.Id,
-								"phone":     msgData.Phone,
-								"status":    status,
-								"details":   details,
-								"timestamp": timestamp,
-							},
-						}
-
-						// Enviar para o webhook
-
-						values, _ := json.Marshal(postmap)
-						data := map[string]string{
-							"jsonData": string(values),
-							"token":    myuserinfo.(Values).Get("Token"),
-						}
-						go callHook(webhookurl, data, msgData.Userid)
-
-						log.Info().Str("id", msgData.Id).Str("status", status).Msg("CallBack processado")
+					// Criar estrutura de evento no mesmo formato do wmiau.go
+					postmap := map[string]interface{}{
+						"type": "CallBack",
+						"event": map[string]interface{}{
+							"id":        msgData.Id,
+							"phone":     msgData.Phone,
+							"status":    status,
+							"details":   details,
+							"timestamp": timestamp,
+						},
 					}
-				} else {
-					log.Warn().Str("userid", fmt.Sprintf("%d", msgData.Userid)).Msg("Nenhum webhook configurado para este usuário")
+
+					// Enviar para o webhook
+
+					values, _ := json.Marshal(postmap)
+					data := map[string]string{
+						"jsonData": string(values),
+						"token":    myuserinfo.(Values).Get("Token"),
+					}
+					go callHook(webhookurl, data, msgData.Userid)
+
+					log.Info().Str("id", msgData.Id).Str("status", status).Msg("CallBack processado")
 				}
+			} else {
+				log.Warn().Str("userid", fmt.Sprintf("%d", msgData.Userid)).Msg("Nenhum webhook configurado para este usuário")
 			}
 		}
 	}
